c6831e218054406216277920a9e4948729064943
ryztar <rizkytangdialla02@gmail.com>
2026-05-22 10:35:43 +0800

    first commit

---
+++ api/index.php
@@ -0,0 +1,4 @@
+<?php
+
+// Mengarahkan request langsung ke file public/index.php bawaan Laravel
+require __DIR__ . '/../public/index.php';
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,8 @@
 <?php
 
-use APP\Http\Middleware\HandlerAppereance;
-use APP\Http\Middleware\HandlerInertiaRequest;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use APP\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,23 +10,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->encryptCookies(except: ['appereance', 'sidebar_state']);
-
-        $middleware->web(append: [
-            HandleAppereance::class,
-            HandleInertiaRequests::class,
-            AddLinkHeadersForPreloadedAssets::class,
-        ]);
-
-        $middleware->trustProxies(
-            '*',
-            Request::HEADER_X_FORWARDED_FOR,
-                Request::HEADER_X_FORWARDED_HOST |
-                Request::HEADER_X_FORWARDED_PORT |
-                Request::HEADER_X_FORWARDED_PROTO
-        );
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
+->withMiddleware(function (Middleware $middleware) {
+    $middleware->alias([
+        'role' => \App\Http\Middleware\CheckRole::class,
+    ]);
+})
+    ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
